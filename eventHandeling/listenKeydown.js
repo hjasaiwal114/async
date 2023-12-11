44391c508a19474ed57f423ef53bdfe9549240e5
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-12-11 14:08:06 +0530

    listen keyword

---
+++ eventHandeling/listenKeydown.js
@@ -0,0 +1,6 @@
+const textInput   = document.getElementById('textInput');
+textInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        console.log('Enter key passed!')
+    }
+})
